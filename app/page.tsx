3b4a102ed687e32c3cb515b724e74f6c37d25bb3
MAC <mac@MACs-MacBook-Pro.local>
2024-04-16 13:39:47 -0400

    added banner compoent

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Banner from "./components/banner";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <h1>Discover Coffee Shops!</h1>
-      <h2>The best coffee shops in town</h2>
+      <Banner />
     </main>
   );
 }
