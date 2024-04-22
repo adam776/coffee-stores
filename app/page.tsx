edbbb27ec58d028144884a33ed27991c586aca3e
MAC <mac@MACs-MacBook-Pro.local>
2024-04-21 20:57:11 -0400

    Added more coffee stores

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,31 @@ import Card from "./components/card.server";
 
 
 export default function Home() {
-  const coffeeStoreId = 'darh-horse-coffee'
+  const coffeeStoreId = 'darh-horse-coffee';
+  const cofeeStores = [
+    {
+      name: 'Beans Coffee',
+      imgUrl: '/static/nafinia-putra-Kwdp-0pok-I-unsplash.jpg'
+    },
+    {
+      name: 'Younges Cafe',
+      imgUrl: '/static/istockphoto-1484464701-1024x1024.jpg'
+    }
+  ]
   return (
     <div className="mb-56">
     <main className="mx-auto mt-10 max-w-6xl px-4">
       <Banner />
+      <div className="mt-20">
+        <h2 className="mt-8 pb-8 text-4xl fontbold text-white">
+          London Stores
+        </h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
-      <Card name="Dark horse coffee" imgUrl="/static/hero-image.png" href={`/coffee-stores/${coffeeStoreId}`} />
+      {cofeeStores.map((cofeeStores, idx) => (
+      <Card key={`${cofeeStores.name}-${idx}`} name={cofeeStores.name} imgUrl={cofeeStores.imgUrl} href={`/coffee-stores/${idx}`} />
+      ))}
       </div>    
+      </div>
     </main>
     </div>
   );
